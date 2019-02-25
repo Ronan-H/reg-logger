87f8dca7b6b6bddba53ac46ae19507981244aaa2
ronan <g00342393@gmit.ie>
2019-02-25 19:46:59 +0000

    Added FrameTimeManager code and corresponding unit tests

---
+++ reg-logger/src/test/java/ronan_tommey/reg_logger/image_processing/FrameTimeManagerTest.java
@@ -0,0 +1,14 @@
+package ronan_tommey.reg_logger.image_processing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FrameTimeManagerTest {
+
+    @org.junit.jupiter.api.Test
+    void addFrameTime() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTimeDuration() {
+    }
+}
