5ef163819ca9c4fe9b06aef48fa31fb5f389d3f9
TommeyFaherty <tommeyf3@hotmail.com>
2019-03-04 18:09:41 +0000

    Additions to the FrameUtilsTest and CarDataUtilsTest tests

--- reg-logger/src/test/java/ronan_tommey/reg_logger/car_data/CarDataUtilsTest.java
+++ reg-logger/src/test/java/ronan_tommey/reg_logger/car_data/CarDataUtilsTest.java
@@ -1,5 +1,37 @@
-import static org.junit.Assert.*;
+package ronan_tommey.reg_logger.car_data;
+
+import org.junit.Ignore;
+import ronan_tommey.reg_logger.image_processing.FrameTimeManager;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CarDataUtilsTest {
 
+    @org.junit.jupiter.api.Test
+    public void generateCarData() throws IOException {
+
+        BufferedImage image = ImageIO.read(new File("./src/test/input/car-data-utils/generate-car-data/pi-cam-image-1.png"));
+        CarData tester = CarDataUtils.generateCarData(image);
+        CarData expected = new CarData(145,226,40,71);
+
+
+        assertEquals(expected,tester);
+    }
+
+    //Need expected CarEstimate
+    @Ignore
+    @org.junit.jupiter.api.Test
+    public void generateCarEstimate() {
+        CarDataSeries seriesTest = new CarDataSeries();
+        FrameTimeManager FTMTest = new FrameTimeManager(10);
+        CarEstimate carTester = CarDataUtils.generateCarEstimate(seriesTest,FTMTest);
+
+
+        //assertEquals(,carTester);
+    }
 }
--- reg-logger/src/test/java/ronan_tommey/reg_logger/image_processing/FrameUtilsTest.java
+++ reg-logger/src/test/java/ronan_tommey/reg_logger/image_processing/FrameUtilsTest.java
@@ -1,5 +1,27 @@
+package ronan_tommey.reg_logger.image_processing;
+
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class FrameUtilsTest {
 
+    @Test
+    public void convertImageToBooleanArray() throws IOException {
+        BufferedImage image = ImageIO.read(new File("./src/test/input/frame-utils/image-to-bool-array/tiny-frame.png"));
+        boolean[] tester = FrameUtils.convertImageToBooleanArray(image);
+        //100101011100
+        boolean[] expected = {true,false,false,true,false,true,false,true,true,true,false,false};
+
+        assertEquals(expected,tester);
+    }
+
+    @Test
+    public void removeNoise() {
+    }
 }
