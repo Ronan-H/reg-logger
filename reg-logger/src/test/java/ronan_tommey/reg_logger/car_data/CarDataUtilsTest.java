8716c43464356f25f2c362f2f8855f84dc62b621
TommeyFaherty <G00344243@gmit.ie>
2019-03-11 12:34:49 +0000

    JUnit test for CaptureWaitEstimator and overall consistency change for tests

--- reg-logger/src/test/java/ronan_tommey/reg_logger/car_data/CarDataUtilsTest.java
+++ reg-logger/src/test/java/ronan_tommey/reg_logger/car_data/CarDataUtilsTest.java
@@ -2,12 +2,13 @@ package ronan_tommey.reg_logger.car_data;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarDataUtilsTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void generateCarData() throws IOException {
 
         BufferedImage image = Utils.readTestImage("./src/test/input/car-data-utils/generate-car-data/pi-cam-image-1.png");
@@ -19,8 +20,8 @@ public class CarDataUtilsTest {
     }
 
     //Need expected CarEstimate
-    @org.junit.jupiter.api.Test
-    @org.junit.jupiter.api.Disabled
+    @Test
+    @Disabled
     public void generateCarEstimate() {
         CarDataSeries seriesTest = new CarDataSeries();
         FrameTimeManager FTMTest = new FrameTimeManager(10);
