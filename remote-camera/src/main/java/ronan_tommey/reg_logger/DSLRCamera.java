34349e9eccd95efed6c43c36830fd6a6c5a5105b
ronan <g00342393@gmit.ie>
2019-03-25 21:41:46 +0000

    Added remote camera code, with some refactorings

--- remote-camera/src/main/java/ronan_tommey/reg_logger/DSLRCamera.java
+++ remote-camera/src/main/java/ronan_tommey/reg_logger/DSLRCamera.java
@@ -1,11 +1,71 @@
 package ronan_tommey.reg_logger;
 
+import org.gphoto2.*;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.FileImageOutputStream;
 import java.awt.image.BufferedImage;
+import java.io.*;
 
 public class DSLRCamera {
+    private long captureTime;
+    private Camera camera;
 
-    public static BufferedImage captureImage(long captureTime){
-        BufferedImage temp = null;
-        return temp;
+    public DSLRCamera(long captureTime, Camera camera) {
+        this.captureTime = captureTime;
+        this.camera = camera;
+    }
+
+    public BufferedImage captureImage(long captureTime) throws IOException {
+        final CameraFile cf2;
+
+        long msSleep = captureTime / 1000000;
+        int nsSleep = (int) (captureTime % 1000000);
+
+        System.out.printf("Received command to capture image after %dms%n", msSleep);
+
+        if (nsSleep > 0) {
+            try {
+                Thread.sleep(msSleep, nsSleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Capturing...");
+
+        cf2 = camera.captureImage();
+
+        String path = "./capture.jpg";
+        File saveFile = new File(path);
+
+        cf2.save(saveFile.getAbsolutePath());
+        CameraUtils.closeQuietly(cf2);
+
+        System.out.println("Image captured and saved as " + path);
+        System.out.println("Reading saved image...");
+
+        BufferedImage capturedImage = null;
+
+        capturedImage = ImageIO.read(saveFile);
+
+        JPEGImageWriteParam jpegParams = new JPEGImageWriteParam(null);
+        jpegParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        jpegParams.setCompressionQuality(1f);
+
+        final ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
+        // specifies where the jpg image has to be written
+        writer.setOutput(new FileImageOutputStream(
+                new File("test.jpg")));
+
+        // writes the file with given compression level
+        // from your JPEGImageWriteParam instance
+        writer.write(null, new IIOImage(capturedImage, null, null), jpegParams);
+
+        return capturedImage;
     }
 }
--- remote-camera/src/main/java/ronan_tommey/reg_logger/RemoteCameraClient.java
+++ remote-camera/src/main/java/ronan_tommey/reg_logger/RemoteCameraClient.java
@@ -1,14 +1,104 @@
 package ronan_tommey.reg_logger;
 
+import org.gphoto2.*;
+
+import javax.imageio.*;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageOutputStream;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
 import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.Socket;
+import java.nio.ByteBuffer;
 
 public class RemoteCameraClient {
 
-    public static void go(){
+    public static final String RASPI_IP = "192.168.0.27";
+    public static final int PORT = 52197;
 
+    private BufferedReader in;
+    private BufferedOutputStream out;
+    private Camera nikon;
+    private boolean running;
+
+    public void go() throws IOException {
+        System.out.println("Starting remote camera client...");
+
+        initCamera();
+        Socket socket;
+
+        socket = new Socket(RASPI_IP, PORT);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new BufferedOutputStream(socket.getOutputStream()) ;
+
+        String line;
+        long captureTime;
+
+        running = true;
+        while (running) {
+            line = in.readLine();
+
+            // line read; capture image at given time
+            captureTime = Long.parseLong(line);
+
+            BufferedImage capturedImage = new DSLRCamera(captureTime, nikon).captureImage(captureTime);
+
+            System.out.println("Uploading captured image...");
+
+            uploadImage(capturedImage);
+        }
     }
 
-    public static void uploadImage(BufferedImage image){
+    private void initCamera() {
+        nikon = null;
 
+        final CameraList cl = new CameraList();
+        System.out.println("Cameras: " + cl);
+        CameraUtils.closeQuietly(cl);
+        nikon = new Camera();
+        nikon.initialize();
+    }
+
+    public void uploadImage(BufferedImage image){
+        try {
+
+            ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+
+            JPEGImageWriteParam jpegParams = new JPEGImageWriteParam(null);
+            jpegParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            jpegParams.setCompressionQuality(1f);
+
+            final ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
+            // specifies where the jpg image has to be written
+            ImageOutputStream imageOutputStream =
+                    new MemoryCacheImageOutputStream(bStream);
+            writer.setOutput(imageOutputStream);
+
+            // writes the image with given compression level
+            // from your JPEGImageWriteParam instance
+            writer.write(null, new IIOImage(image, null, null), jpegParams);
+
+            imageOutputStream.flush();
+            imageOutputStream.close();
+            byte[] imgBytes = bStream.toByteArray();
+
+            System.out.println("Image size (bytes): " + imgBytes.length);
+            byte[] size = ByteBuffer.allocate(4).putInt(bStream.size()).array();
+
+            out.write(size);
+            out.write(imgBytes);
+            out.flush();
+            System.out.println("Finished.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            new RemoteCameraClient().go();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
