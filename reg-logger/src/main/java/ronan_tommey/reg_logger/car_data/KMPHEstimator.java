174294b19354c31fd9d0566ca1f8442e5a001ed3
TommeyFaherty <tommeyf3@hotmail.com>
2019-04-27 20:07:23 +0100

    JavaDoc and commenting for KMPHEstimator

--- reg-logger/src/main/java/ronan_tommey/reg_logger/car_data/KMPHEstimator.java
+++ reg-logger/src/main/java/ronan_tommey/reg_logger/car_data/KMPHEstimator.java
@@ -1,5 +1,8 @@
 package ronan_tommey.reg_logger.car_data;
 
+/**
+ * Estimates the speed of the car in kilometers per hour from a series of frames
+ */
 public class KMPHEstimator {
     private double pixelsPerMeter;
 
@@ -7,6 +10,11 @@ public class KMPHEstimator {
         pixelsPerMeter = frameWidth / imageWidthMeters;
     }
 
+    /**
+     *Estimates the speed in kilometer per hour
+     * @param pixelsPerFrame The number of pixels in a frame
+     * @param frameTimeManager The difference in time between each frame
+     */
     public double getKMPHEstimate(double pixelsPerFrame, FrameTimeManager frameTimeManager) {
         double pixelsPerSecond = pixelsPerFrame * (1000000000d / frameTimeManager.getAverageFrameDelta(4));
         double metersPerSecond = pixelsPerSecond / pixelsPerMeter;
