1ab880258643cf723af55b64e90f0c01dc9e65e6
ronan <g00342393@gmit.ie>
2019-04-27 21:33:03 +0100

    Added Javadoc and comments for CarPassFileSystem

--- reg-logger/src/main/java/ronan_tommey/reg_logger/reg_logging/CarPassLogger.java
+++ reg-logger/src/main/java/ronan_tommey/reg_logger/reg_logging/CarPassLogger.java
@@ -1,5 +1,9 @@
 package ronan_tommey.reg_logger.reg_logging;
 
+/**
+ * Interface for classes which can log log car pass details in
+ * some way (eg. to a database or a file system)
+ */
 public interface CarPassLogger {
     void logPass(CarPassDetails carPassDetails);
 }
