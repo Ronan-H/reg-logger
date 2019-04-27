8927d2ee6f2d1ba927b4ba108378e53e18b6832a
ronan <g00342393@gmit.ie>
2019-04-27 21:01:31 +0100

    Added Javadoc and comments for the remaining CaptureWaitEstimator code

--- reg-logger/src/main/java/ronan_tommey/reg_logger/CaptureWaitEstimator.java
+++ reg-logger/src/main/java/ronan_tommey/reg_logger/CaptureWaitEstimator.java
@@ -5,6 +5,11 @@ import ronan_tommey.reg_logger.image_processing.CarPassImageDump;
 
 import java.io.IOException;
 
+/**
+ * Uses a history of CarData objects and frame delta values
+ * to produce an estimate for how long a passing car will take
+ * to reach the specified capture point
+ */
 public class CaptureWaitEstimator {
     // minimum distance the car has to be from where it started
     // before an estimate can be generated, when coming from the left
@@ -23,12 +28,20 @@ public class CaptureWaitEstimator {
     private CarPassImageDump carPassImageDump;
     private boolean allowCapture;
 
+    /**
+     * @param numEstimateFrames Number of frames to use to produce the estimate (last n frames)
+     * @param capturePoint The x position of the frame where the reg of the car
+     *                     should be when the image should be captured
+     * @param captureLatency The total time it takes to actually capture an image of the passing car's reg plate
+     * @param carPassImageDump Associated CarPassImageDump object, to trigger the dumping of frames. Can be null.
+     */
     public CaptureWaitEstimator(int numEstimateFrames, int capturePoint, int imageWidth, long captureLatency, CarPassImageDump carPassImageDump) {
         this.numEstimateFrames = numEstimateFrames;
         this.capturePoint = capturePoint;
         this.imageWidth = imageWidth;
         this.captureLatency = captureLatency;
         this.carPassImageDump = carPassImageDump;
+
         carDataSeries = new CarDataSeries(imageWidth);
         // TODO: don't hard code buffer size
         frameTimeManager = new FrameTimeManager(350);
@@ -96,28 +109,54 @@ public class CaptureWaitEstimator {
         this.lastMovingPixels = movingPixels;
     }
 
+    /**
+     * Method to be called when a wait estimate is locked in, to prevent
+     * further images from attempting to be captured
+     */
     public void onCapture(){
         allowCapture = false;
     }
 
+    /**
+     * @return True if this object is ready to produce a capture wait estimate for a passing car
+     */
     public boolean estimateReady(){
-        return carDataSeries.size() >= numEstimateFrames
-                && allowCapture
+        return carDataSeries.size() >= numEstimateFrames // ensure enough frames are stored to generate an estimate
+                && allowCapture // no estimate if capture is not allowed
+                // ensure car is fully in frame if it's coming from the left
                 && (!carDataSeries.isGoingRight() ||
                 carDataSeries.getLast().getLeftX() - carDataSeries.getFirst().getLeftX() > minDistBeforeInFrame);
     }
 
+    /**
+     * Delegate method; calls a utility method to generate the CarEstimate
+     * based on the history of CarData objects and frame deltas
+     * @return The generated CarEstimate object
+     */
     public CarEstimate generateCarEstimate(){
         lastCarEstimate = CarDataUtils.generateCarEstimate(carDataSeries, numEstimateFrames, frameTimeManager, true);
         return lastCarEstimate;
     }
 
+    /**
+     * Generates an estimate of how long it will take the passing car's
+     * registration plate to reach the capture point, in nanoseconds. Also
+     * takes into accunt the capure latency.
+     * @return Capture wait esatimate (nanoseconds)
+     */
     public long getWaitEstimate(){
+        // get estimate of where the car's registration plate was in the most recent frame
         int regPosEstimate = carDataSeries.getLast().getRegPosEstimate();
+        // calculate the car's distance to the capture point based on the reg position estimate
         int distToCapturePoint = capturePoint - regPosEstimate;
+        // calculate the number of frames before the car reaches the capture point
+        // formula: (time = distance / speed)
         double framesTillCapture = distToCapturePoint / lastCarEstimate.getPixelSpeed();
+        // get the average time between frames for the number of frames we're using for the estimatee
         long avgFrameDelta = frameTimeManager.getAverageFrameDelta(numEstimateFrames);
 
+        // multiply frames until capture by average frame delta to get the capture wait time in nanoseconds
+        // also account for capture latency by taking it away
         return (long)(framesTillCapture * avgFrameDelta) - captureLatency;
     }
 }
