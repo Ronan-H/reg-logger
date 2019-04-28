d702a3219e121be55a1c3cccee78c8bc35356ccc
ronan <g00342393@gmit.ie>
2019-04-28 18:41:29 +0100

    Added Javadoc and comments for remaining reg-logger classes

--- reg-logger/src/main/java/ronan_tommey/reg_logger/reg_logging/ALPR.java
+++ reg-logger/src/main/java/ronan_tommey/reg_logger/reg_logging/ALPR.java
@@ -2,8 +2,17 @@ package ronan_tommey.reg_logger.reg_logging;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Class used for abstracted use of OpenALPR
+ */
 public class ALPR {
 
+    /**
+     * Uses OpenALPR to convert an image of a registration plate to the reg text
+     * as a string
+     * @param carImage Image containing the car's registration plate
+     * @return Registration plate text
+     */
     public static String extractRegText(BufferedImage carImage) {
         return null;
     }
